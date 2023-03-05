3323c739f2acac209af6b0b3260986968d421bf0
Darshan <darshan.kb@outlook.com>
2023-03-05 23:07:38 +0530

    Countdown application created

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
 const http = require("http"); // creating http server
+const app = require("express")();
+app.listen(9091, ()=>console.log("listening on http port 9091"));
+app.get("/",(req,res)=> res.sendFile(_dirname+"/index.html"));
 
 const webSocketServer = require("websocket").server; //creating websocket server this will give a class which contains all the events
 
 let connection = null;
-let count = 1000;
+let count = 60;
 const httpserver = http.createServer((req, res) =>{
     console.log("we have received a request");
 })
-
+httpserver.listen(8230, () => console.log("My server is listening 8230"));
+const clients = {};
+const clientarry=[];
 const websocket = new webSocketServer({ //It takes the JSON. 
     "httpServer" : httpserver           //we have to pass httpserver object to it. Its just the handshake part. httpserver has the socket for the TCP connection
 })
+
 
 websocket.on("request", request=>{
     //console.log("hello");
@@ -20,16 +26,50 @@ websocket.on("request", request=>{
     connection.on("message", message=>{                             // this method is called when we call ws.send from the client
         //console.log(`received message ${message.utf8Data}`);
         
-        //connection.send("Hello client!!");
+        connection.send("Hello client!!");
+        //sendevery3sec();
     }) 
-    var x = setInterval(function(){
-        console.log(count);
-        connection.send(count);
-        count-=1;
-        if(count<0){
-            clearInterval(x);
-        }
-    },1000)
+
+    const clientId = guid(); 
+    clients[clientId] = {
+        "connection" : connection
+    }
+    clientarry.push(clientId);
+    const payload ={
+        "method": "connect",
+        "clientId": clientId
+    }
+    //connection.send(JSON.stringify(payload));
+    
+    sendevery3sec();
 })
 
-httpserver.listen(8230, () => console.log("My server is listening 8230"));
+
+
+function guid() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+
+  var x = setInterval(function(){
+    console.log(count);
+    
+    count-=1;
+    if(count<0){
+        clearInterval(x);
+    }
+},1000);
+
+function sendevery3sec(){
+    const payload ={
+        "method": "countdown",
+        "count": count
+    }
+    clientarry.forEach(function(item){
+        clients[item].connection.send(JSON.stringify(payload));
+    })
+    //connection.send(count);
+    if(count<0)
+        return;
+    setTimeout(sendevery3sec,1000);
+}
+  
